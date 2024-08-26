191d7eca2346757b5f035bc8d937af29aa5fbe94
TanhBb1911 <tanh191202@gmail.com>
2024-08-26 14:09:32 +0700

    first

---
+++ components/guide.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Guide = () => {
+    return (
+        <section className="flexCenter flex-col">
+            <div className="padding-container max-container w-full pb-24">
+                <Image src="/camp.svg" alt="camp" width={50} height={50} />
+                <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+                    We are here for you
+                </p>
+                <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+                    <h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">Guide You to Easy Path</h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Guide
